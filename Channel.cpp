8715d2678d14b6412d101899ba873904325f56e6
tw1st <twist_taylor753@163.com>
2025-08-04 14:17:30 +0800

    feat(network): 添加用于事件处理的Channel类 -实现Channel类处理文件描述符事件 -支持读/写/销毁回调函数 -允许启用/禁用写事件 -包含FDEvent枚举描述事件类型

---
+++ Channel.cpp
@@ -0,0 +1,25 @@
+﻿#include "Channel.h"
+
+Channel::Channel(int fd, int events, handleFunc readFunc, handleFunc writeFunc, handleFunc destroyFunc, void* arg)
+	:fd(fd), events(events), 
+	readCallback(readFunc), writeCallback(writeFunc), destroyCallback(destroyFunc), arg(arg)
+{
+
+}
+
+void Channel::setWriteEventEnable(bool flag)
+{
+    if (flag)
+    {
+        this->events |= static_cast<int>(FDevent::WriteEvent);
+    }
+    else
+    {
+        this->events &= static_cast<int>(FDevent::WriteEvent);
+    }
+}
+
+bool Channel::isWriteEventEnable()
+{
+    return this->events & FDevent::WriteEvent;
+}
---
+++ Channel.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <stdbool.h>
+
+//函数指针
+using handleFunc = int(*)(void* arg);
+
+//fd读写事件
+enum class FDevent
+{
+	TimeOut = 0x01,
+	ReadEvent = 0x02,
+	WriteEvent = 0x04
+};
+
+class Channel
+{
+public:
+	Channel(int fd, int events, handleFunc readFunc, handleFunc write, handleFunc destroy, void* arg);
+	void setWriteEventEnable(bool flag);
+	bool isWriteEventEnable();
+	//事件回调函数
+	handleFunc readCallback;
+	handleFunc writeCallback;
+	handleFunc destroyCallback;
+private:
+	int fd;		//通信文件描述符
+	int events;	//事件
+	void* arg;	//回调函数参数
+};
